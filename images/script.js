ec1f85b0348bb761f5b49b105fd2818ed4fc2e3b
ylem76 <ylem76@thebreeze.co.kr>
2022-01-25 16:48:13 +0900

    darkmode modify

--- images/script.js
+++ images/script.js
@@ -1,45 +1,53 @@
-// function dark() {
-//   var theme = localStorage.getItem('theme');
+// 다크모드
+function darkMode() {
+    // 테마 가져오기
+    var storedTheme = localStorage.getItem('theme');
+	var prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+	var themeType = storedTheme;
 
-//   if (!theme) {
-//   localStorage.setItem('theme', 'light');
-//   }
+    // 저장된 테마가 없으면 선호 모드에 따라 로컬 스토리지 셋팅
+	if(!storedTheme) {
+        if(prefersDarkMode == false) {
+        	themeType = 'light';
+        } else {
+        	themeType = 'dark';
+        }
+        localStorage.setItem('theme', themeType);
+	}
 
-//   document.body.classList.toggle('color-dark', theme === 'dark');
-//   document.querySelector('.toggle-button').onclick = function() {
-//   var theme = localStorage.getItem('theme');
-//   localStorage.setItem('theme', `${theme === 'dark' ? 'light' : 'dark'}`);
-//   document.body.classList.toggle('color-dark');
-//   }
+	// 데이터에 따라 html attribute 지정
+	document.documentElement.setAttribute('theme-color', themeType);
+}
 
-//   setTimeout(() => {
-//   document.body.style.visibility = 'visible';
-//   }, 300);
-// }
+darkMode();
 
-// document.addEventListener('DOMContentLoaded', () => {
-// dark();
 
-// });
 
 (function ($) {
-	function scroll() {
-		var scrollBar = document.querySelector('.scrollbar');
-
-		window.addEventListener('scroll', function () {
+	function scrollIndicator() {
+		function getScrollData(scrollBar) {
+			console.log(scrollBar);
 			var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-			// const scrollHeight = document.documentElement.scrollHeight || document.body.scrollHeight;
 			var scrollHeight = $('.reply-btn-wrap').offset().top;
 			var clientHeight = document.documentElement.clientHeight || document.body.clientHeight;
 			var contentHeight = scrollHeight - clientHeight;
 			var percent = (scrollTop / contentHeight) * 100;
-
 			scrollBar.style.transform = `translateX(-${100 - percent}%)`;
-		});
+		}
+
+		$(document).ready(function() {
+			if($('article').length === 0) {return}
+			var scrollBar = document.querySelector('.scrollbar');
+			getScrollData(scrollBar);
+			window.addEventListener('scroll', function() {
+				getScrollData(scrollBar)
+			});		
+		});		
 	}
-	if ($('#article').length !== 0 ) {
-		scroll();
-	}
+
+	scrollIndicator();
+
+	
 
 	function gnb() {
 		$('#header').on('click', '.mobile-menu', function () {

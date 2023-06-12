dceec7462d634a69c5a274b0feb286fea3c9007c
ylem76 <ylem76@thebreeze.co.kr>
2023-06-12 12:06:12 +0900

    before

--- images/toc_test.js
+++ images/toc_test.js
@@ -1,60 +1,44 @@
 $(document).ready(function () {
-  // 클래스 이름 입력
-  var inputVal = {
-    entryName: "entry-content", // 게시글 내용 출력 부분
-    entryWrapName: "entry-wrap", // 게시글 내용 출력 감싸는 부분(toc넣을 곳)
-    navName: "toc-list", // 네비게이션 리스트 이름 지정
-    headerName: "header", // 상단 헤더 클래스 이름
-    gap: 40, // toc 상단 위치값
-  };
-
-  function checkArticleStructure() {
-    if (
-      document.querySelector("." + inputVal.entryWrapName) === null ||
-      document.querySelector("." + inputVal.entryName) === null ||
-      document.querySelector("." + inputVal.navName) === null
-    ) {
-      return false;
-    }
-  }
-
-  if (checkArticleStructure() !== false) {
-    tocInit(inputVal);
-  } else {
-    console.log("no structure");
+  var entryName = "entry-content"; // 게시글 내용 출력 부분
+  if (document.querySelector("." + entryName) !== null) {
+    tocInit();
   }
 });
 
-function tocInit(inputVal) {
-  var tocVars = {
+function tocInit() {
+  // 사용자 커스텀
+  var entryName = "entry-content"; // 게시글 내용 출력 부분
+  var entryWrapName = "entry-wrap"; // 게시글 내용 출력 감싸는 부분(toc넣을 곳)
+  var navName = "toc-list"; // 네비게이션 리스트 이름 지정
+  var headerName = "header"; // 상단 헤더 클래스 이름
+  var gap = 40; // toc 상단 위치값
+
+  var TOC_CONST = {
     navItem: [],
     offsetTops: [],
-    mainWrap: document.querySelector("." + inputVal.entryWrapName),
-    contentWrap: document.querySelector("." + inputVal.entryName),
-    navWrap: document.querySelector("." + inputVal.navName),
+    mainWrap: document.querySelector("." + entryWrapName),
+    contentWrap: document.querySelector("." + entryName),
+    navWrap: document.querySelector("." + navName),
     headings: [],
     newHeadings: [],
     navItemArr: [],
     headerHeight:
-      inputVal.headerName != "" &&
-      document.querySelector("." + inputVal.headerName)
-        ? document.querySelector("." + inputVal.headerName).offsetHeight
+      headerName != ""
+        ? document.querySelector("." + headerName).offsetHeight
         : 0,
     scriptScroll: false,
-    gap: inputVal.gap,
   };
   var scriptScroll = false;
 
   function getHeadingData() {
-    var contentWrap = tocVars.contentWrap;
-
-    var offsetTops = tocVars.offsetTops;
-    var newHeadings = tocVars.newHeadings;
+    var contentWrap = TOC_CONST.contentWrap;
+    var offsetTops = TOC_CONST.offsetTops;
+    var newHeadings = TOC_CONST.newHeadings;
 
     var headings = contentWrap
       ? contentWrap.querySelectorAll("h1, h2, h3, h4")
       : [];
-    tocVars.headings = headings;
+    TOC_CONST.headings = headings;
 
     Array.prototype.forEach.call(headings, function (item, index, headings) {
       item.id = "toc-link-" + index;
@@ -65,9 +49,9 @@ function tocInit(inputVal) {
       offsetTops.push(
         parseInt(
           item.offsetTop +
-            tocVars.mainWrap.offsetTop -
-            tocVars.headerHeight -
-            tocVars.gap
+            TOC_CONST.mainWrap.offsetTop -
+            TOC_CONST.headerHeight -
+            gap
         )
       );
       newHeadings.push({
@@ -75,67 +59,72 @@ function tocInit(inputVal) {
         index: parseInt(item.localName.substr(1)),
         text: item.innerText,
         id: item.id,
-        top: item.offsetTop + tocVars.headerHeight + tocVars.mainWrap.offsetTop,
+        top:
+          item.offsetTop +
+          TOC_CONST.headerHeight +
+          TOC_CONST.mainWrap.offsetTop,
       });
     });
   }
-
   function renderToc() {
     // 리스트 html 만들고 렌더링
-    function makeHtmlFrag(newHeadings) {
+    var newHeadings = TOC_CONST.newHeadings;
+    var mainWrap = TOC_CONST.mainWrap;
+    var listHtml = "";
+    newHeadings.forEach(function (item, idx, newHeadings) {
       var html = "";
-      newHeadings.forEach(function (item, idx, newHeadings) {
-        var prevLevel = idx > 0 ? newHeadings[idx - 1].index : 0;
-        var currentLevel = item.index;
-        var levelGap = prevLevel - currentLevel;
-
-        // start idx
-        if (idx === 0) {
-          for (var i = 1; i < currentLevel; i++) {
-            html += '<ul class="lv-' + i + '"><li>';
-          }
-          html += '<ul class="lv-' + currentLevel + '">';
+      if (idx > 0) {
+        var prevLevel = newHeadings[idx - 1].index;
+      }
+      var currentLevel = item.index;
+      if (idx == 0 || prevLevel - currentLevel == -1) {
+        html += '<ul class="lv-' + currentLevel + '">';
+      } else if (prevLevel - currentLevel == -2) {
+        html += "<ul><li>";
+        html += '<ul class="lv-' + currentLevel + '">';
+      } else if (prevLevel - currentLevel == -3) {
+        html += "<ul><li><ul><li>";
+        html += '<ul class="lv-' + currentLevel + '">';
+      } else if (prevLevel - currentLevel == 1) {
+        html += "</li></ul>";
+      } else if (prevLevel - currentLevel == 2) {
+        html += "</li></ul>";
+        html += "</li></ul>";
+      } else if (prevLevel - currentLevel == 3) {
+        html += "</li></ul>";
+        html += "</li></ul>";
+        html += "</li></ul>";
+      }
+      html +=
+        '<li><a class="toc-item" href="#' +
+        item.id +
+        '" target-idx =' +
+        idx +
+        " >" +
+        item.text +
+        "</a>";
+      var arrayLen = newHeadings.length - 1;
+      if (idx == arrayLen) {
+        if (currentLevel == 1) {
+          html += "</li></ul>";
+        } else if (currentLevel == 2) {
+          html += "</li></ul></li></ul>";
+        } else if (currentLevel == 3) {
+          html += "</li></ul></li></ul></li></ul>";
+        } else if (currentLevel == 4) {
+          html += "</li></ul></li></ul></li></ul>";
         }
-
-        // middle idx
-        if (idx > 0 && levelGap < 0) {
-          for (var i = 1; i < Math.abs(levelGap); i++) {
-            html += "<ul><li>";
-          }
-          html += '<ul class="lv-' + currentLevel + '">';
-        }
-        if (idx > 0 && levelGap > 0) {
-          for (var i = 0; i < levelGap; i++) {
-            html += "</li></ul>";
-          }
-        }
-        html +=
-          '<li><a class="toc-item" href="#' +
-          item.id +
-          '" target-idx =' +
-          idx +
-          " >" +
-          item.text +
-          "</a>";
-        var arrayLen = newHeadings.length - 1;
-
-        // end idx
-        if (idx === arrayLen) {
-          for (var i = 0; i < currentLevel; i++) {
-            html += "</li></ul>";
-          }
-        }
-      });
-      return html;
-    }
-
-    tocVars.navWrap.classList.add(inputVal.navName);
-    tocVars.navWrap.innerHTML = makeHtmlFrag(tocVars.newHeadings);
-    tocVars.mainWrap.insertBefore(tocVars.navWrap, null);
+      }
+      listHtml += html;
+    });
+    var newNode = document.createElement("nav");
+    newNode.classList.add(navName);
+    newNode.innerHTML = listHtml;
+    TOC_CONST.navWrap = newNode;
+    mainWrap.insertBefore(newNode, null);
   }
-
   function changeCurrent(targetIndex) {
-    var navItemArr = tocVars.navItemArr;
+    var navItemArr = TOC_CONST.navItemArr;
     navItemArr.forEach(function (item, index) {
       if (index !== targetIndex) {
         item.classList.remove("on");
@@ -144,51 +133,45 @@ function tocInit(inputVal) {
       }
     });
   }
-
   function onClickMove() {
-    function onClickHandle(e) {
-      e.preventDefault();
-
-      var targetElem = e.target;
-      if (targetElem.tagName != "A") {
-        return;
-      }
-      var targetIdx = parseInt(targetElem.attributes["target-idx"].value);
-      tocVars.scriptScroll = true;
-      changeCurrent(targetIdx);
-      console.log(tocVars.offsetTops[targetIdx]);
-      $("html, body").animate(
-        {
-          scrollTop: tocVars.offsetTops[targetIdx],
-        },
-        300,
-        function () {
-          tocVars.scriptScroll = false;
-        }
-      );
-    }
     // 클릭 시 이동
-    var navItem = tocVars.navWrap.querySelectorAll("a");
-    var navItemArr = tocVars.navItemArr;
+    var navWrap = TOC_CONST.navWrap;
+    var navItem = navWrap.querySelectorAll("a");
+    var navItemArr = TOC_CONST.navItemArr;
     Array.prototype.forEach.call(navItem, function (item) {
       navItemArr.push(item);
     });
 
-    tocVars.navWrap.addEventListener("click", function (e) {
-      onClickHandle(e);
+    navWrap.addEventListener("click", function (e) {
+      var targetElem = e.target;
+      var offsetTops = TOC_CONST.offsetTops;
+      e.preventDefault();
+      if (targetElem.tagName != "A") {
+        return;
+      }
+      var targetIdx = parseInt(targetElem.attributes["target-idx"].value);
+      TOC_CONST.scriptScroll = true;
+      changeCurrent(targetIdx);
+      $("html, body").animate(
+        { scrollTop: offsetTops[targetIdx] },
+        300,
+        function () {
+          TOC_CONST.scriptScroll = false;
+        }
+      );
     });
   }
 
   function onScrollMove(scriptScroll) {
     // 스크롤 이벤트
-    var navItemArr = tocVars.navItemArr;
+    var navItemArr = TOC_CONST.navItemArr;
     var tocOnScroll = function (e) {
       var scrollTop = e.target.scrollingElement.scrollTop;
-      if (tocVars.scriptScroll == true) {
+      if (TOC_CONST.scriptScroll == true) {
         return;
       } else {
         var targetIndex = 0;
-        var offsetTops = tocVars.offsetTops;
+        var offsetTops = TOC_CONST.offsetTops;
         offsetTops.forEach(function (item, idx) {
           if (scrollTop >= item) {
             targetIndex = idx;
@@ -216,10 +199,10 @@ function tocInit(inputVal) {
       if (e.target.innerWidth < 767) {
         return;
       }
-      Array.prototype.forEach.call(tocVars.headings, function (item) {
+      Array.prototype.forEach.call(TOC_CONST.headings, function (item) {
         offsetTops.push(parseInt(item.offsetTop));
       });
-      tocVars.headerHeight =
+      TOC_CONST.headerHeight =
         headerName != ""
           ? document.querySelector("." + headerName).offsetHeight
           : 0;
@@ -235,17 +218,19 @@ function tocInit(inputVal) {
 
   function headerFix() {
     var scrollTop = 0;
-    var tocNav = tocVars.navWrap;
-    var tocNavUl = tocVars.navWrap.childNodes[0];
+    var tocNav = TOC_CONST.navWrap;
+    var tocNavUl = TOC_CONST.navWrap.childNodes[0];
     var $contentWrap = $(".entry-content");
-    var fixOffset =
-      $contentWrap.offset().top - tocVars.headerHeight - tocVars.gap;
+    var fixOffset = $contentWrap.offset().top - TOC_CONST.headerHeight - gap;
     var endOffset =
-      $contentWrap.offset().top + $contentWrap.height() - tocNavUl
-        ? tocNavUl.offsetHeight
-        : 0 - tocVars.headerHeight - tocVars.gap - tocVars.gap;
+      $contentWrap.offset().top +
+      $contentWrap.height() -
+      tocNavUl.offsetHeight -
+      TOC_CONST.headerHeight -
+      gap -
+      gap;
 
-    console.log($contentWrap.offset().top, tocVars.headerHeight, tocVars.gap);
+    console.log($contentWrap.offset().top, TOC_CONST.headerHeight, gap);
     console.log(fixOffset);
     $(window).on("scroll", function (e) {
       scrollTop = e.target.scrollingElement.scrollTop;
@@ -254,7 +239,7 @@ function tocInit(inputVal) {
         tocNavUl.style.top = "0";
       } else if (scrollTop < endOffset) {
         tocNavUl.style.position = "fixed";
-        tocNavUl.style.top = tocVars.headerHeight + gap + "px";
+        tocNavUl.style.top = TOC_CONST.headerHeight + gap + "px";
         tocNavUl.style.bottom = "auto";
       } else if (scrollTop > endOffset) {
         tocNavUl.style.position = "absolute";
